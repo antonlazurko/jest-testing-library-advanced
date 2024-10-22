9ac9a08276441eacba9d6668389418b5d35b76cc
Anton Lazurko <antla@ciklum.com>
2024-10-22 13:17:54 +0300

    feat: mock modules

---
+++ src/__mocks__/todos.mock.js
@@ -0,0 +1,1 @@
+export const mockTodo = { id: 1, title: 'Todo 1', completed: false };
---
+++ src/__tests__/createTodo.test.js
@@ -0,0 +1,51 @@
+import { mockTodo } from '../__mocks__/todos.mock';
+import { createTodo, createTodoOnServer } from '../createTodo';
+
+const mockedV4 = jest.fn(() => 'uniqueId1')
+
+jest.mock('uuid', () => ({
+  ...jest.requireActual('uuid'),
+  v4: () => mockedV4(),
+}))
+
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    ok: true,
+    json: () => Promise.resolve(mockTodo),
+  })
+);
+describe('createTodo testing', () => {
+
+afterEach(() => {
+  jest.clearAllMocks();
+})
+
+  test('should create todo', () => {
+    const title = 'Todo 1'
+    const expectedResult = {
+      title,
+      completed: false,
+      id: 'uniqueId1',
+    }
+    const todo = createTodo(title)
+    expect(mockedV4).toHaveBeenCalledTimes(1)
+    expect(todo).toEqual(expectedResult)
+  })
+
+  test('should create todo on server', async () => {
+    const result = await createTodoOnServer('Todo 1')
+    expect(result).toEqual(mockTodo)
+    expect(fetch).toHaveBeenCalledTimes(1)
+  })
+
+  test('should throw an error when if fetch fails', async () => {
+    fetch.mockRejectedValueOnce('uniqueId2')
+    await expect(createTodoOnServer('Todo 1')).rejects.toMatch('uniqueId2')
+  })
+
+  test('should throw an error when response is not ok', async () => {
+    fetch.mockResolvedValueOnce({ ok: false })
+    await expect(createTodoOnServer('Todo 1')).rejects.toMatch('Cannot create todo')
+  })
+
+})
