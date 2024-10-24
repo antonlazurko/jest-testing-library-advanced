ae410f2c4f20b7270d792c44e5ec6007ccf1af0a
Anton Lazurko <antla@ciklum.com>
2024-10-24 17:02:42 +0300

    feat: add tests todos

---
+++ src/__tests__/validate-password.test.js
@@ -0,0 +1,10 @@
+    import {validatePassword} from "../validate-password.js";
+
+describe('validatePassword testing', () => {
+    test('should return true when password is valid')
+    test.todo('should validate password against 8 characters minimum')
+    test.todo('should validate password against 1 uppercase letter')
+    test.todo('should validate password against mixed case')
+    test.todo('should validate password against digits and characters')
+    test.todo('should validate password against 1 special character')
+})
