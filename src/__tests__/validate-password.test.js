87d0bb31508a08a542880c9bd571dc5a1831cada
Anton Lazurko <antla@ciklum.com>
2024-10-24 17:02:57 +0300

    feat: add tests

--- src/__tests__/validate-password.test.js
+++ src/__tests__/validate-password.test.js
@@ -1,10 +1,34 @@
-    import {validatePassword} from "../validate-password.js";
+import {validatePassword} from "../validate-password.js";
 
 describe('validatePassword testing', () => {
-    test('should return true when password is valid')
-    test.todo('should validate password against 8 characters minimum')
-    test.todo('should validate password against 1 uppercase letter')
-    test.todo('should validate password against mixed case')
-    test.todo('should validate password against digits and characters')
-    test.todo('should validate password against 1 special character')
+    test('should return true when password is valid', () => {
+        const password = 'password123'
+        const result = validatePassword(password)
+        expect(result).toBe(true)
+    })
+    test('should validate password against 8 characters minimum', () => {
+        const password = 'pass'
+        const result = validatePassword(password)
+        expect(result).toBe(false)
+    })
+    test('should validate password against 1 uppercase letter', () => {
+        const password = 'password123'
+        const result = validatePassword(password)
+        expect(result).toBe(false)
+    })
+    test('should validate password against mixed case', () => {
+        const password = 'Password123'
+        const result = validatePassword(password)
+        expect(result).toBe(false)
+    })
+    test('should validate password against digits and characters', () => {
+        const password = 'Password123'
+        const result = validatePassword(password)
+        expect(result).toBe(false)
+    })
+    test('should validate password against 1 special character', () => {
+        const password = 'Password123'
+        const result = validatePassword(password)
+        expect(result).toBe(false)
+    })
 })
