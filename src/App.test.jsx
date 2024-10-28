74ab1c86fffa4e1787e8588cf5221c7126a3cf6c
Anton Lazurko <antla@ciklum.com>
2024-10-28 10:25:38 +0200

    feat: App mock server request

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,10 +1,13 @@
 import { fireEvent, act, render, screen } from "@testing-library/react";
 import { passwordValidationErrors } from './constants/validation';
 
-
 import App from "./App";
+import * as waitMock from './helpers/wait';
+
+const waitSpy = jest.spyOn(waitMock, 'wait');
 
 describe("App", () => {
+
     test("should render App with form and title", () => {
         const {container} = render(<App />);
         expect(screen.getByTestId('app')).toBeInTheDocument()
@@ -39,5 +42,30 @@ describe("App", () => {
         })
         const errorMessageAfterSubmit = await screen.findByText(passwordValidationErrors.length);
         expect(errorMessageAfterSubmit).toBeInTheDocument();
+    })
+
+    test('should render App with success message when form was submit with strong  password', async () => {
+        render(<App />);
+        const userNameInput = screen.getByLabelText(/User name/);
+        const passwordInput = screen.getByLabelText(/Password/);
+        const submitButton = screen.getByRole('button', { name: /Create user/ });
+
+        const successMessage = screen.queryByText(/created with password/);
+        const errorMessage = screen.queryByText(passwordValidationErrors.length);
+
+        expect(successMessage).not.toBeInTheDocument();
+        expect(errorMessage).not.toBeInTheDocument();
+
+        const promise = Promise.resolve();
+
+        waitSpy.mockImplementationOnce(() => (promise));
+
+        act(() => {
+            fireEvent.change(userNameInput, {target: {value: 'test'}});
+            fireEvent.change(passwordInput, {target: {value: '1234567Pp!'}});
+            fireEvent.click(submitButton);
+        })
+        const successMessageAfterSubmit = await screen.findByText(/created with password/);
+        expect(successMessageAfterSubmit).toBeInTheDocument();
     })
 })
